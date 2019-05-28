de519cf5b7c044f9466d411e611c216a4d5314ec
Gilbert Macabuag <gmacabuag@bned.com>
2019-05-28 14:25:48 -0400

    Added Redux support

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@
 // const Home = require("./client/components/Home").default;
 // const app = express();
 
+import "babel-polyfill";
 import express from "express";
 import renderer from "./helpers/renderer";
+import createStore from "./helpers/createStore";
 
 const app = express();
 
 app.use(express.static("public"));
 app.get("*", (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+
+  // some logic to initialize
+  // and load data into the store
+
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
